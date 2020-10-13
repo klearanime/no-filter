38d4a69280a009fdc058321c1c6d06dd633aba80
Crystal K Young <maybiz@hotmail.com>
2020-10-13 14:12:27 -0400

    done

--- main.js
+++ main.js
@@ -2,10 +2,45 @@
  * YOUR CODE HERE *
  ******************/
 
+function onlyOdds(nums) {
+  const result = []
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 1) {
+      result.push(nums[i])
+    }
+  }
+  return result
+}
 
+function onlyEvens(nums) {
+  const result = []
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) {
+      result.push(nums[i])
+    }
+  }
+  return result
+}
 
+function shortNamesOnly(names) {
+  const result = []
+  for (let i = 0; i < names.length; i++) {
+    if (names[i].length < 7) {
+      result.push(names[i])
+    }
+  }
+  return result
+}
 
-
+function sNames(name) {
+  const result = [];
+  for (const names of name) {
+    if (names[0] === 'S') {
+      result.push(names)
+    }
+  }
+  return result;
+}
 
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
